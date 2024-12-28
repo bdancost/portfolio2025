c405c9888aa29728c933889aa05639f4f4316e57
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-12-28 20:23:03 -0300

    Adicionando animaçoes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,52 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
+  let textoH1 = ["Daniel ", "Fernandes"];
+  let textoP =
+    "Olá, sou Daniel, estudante do 5º semestre de Análise e Desenvolvimento de Sistemas (ADS) tenho 36 anos. Tenho conhecimento prático com HTML, CSS, Javascript, Tailwind, TypeScript, React, Python e SQL, e estou em constante aprendizado para me aperfeiçoar como desenvolvedor Front-End.";
+
+  let tituloElemento = document.querySelector(".titulo-principal");
+  let paragrafoElemento = document.querySelector(".descricao-autor");
+
+  let atraso = 150;
+  let atrasoParagrafo = 30;
+
+  let contadorDeLetras = 0;
+  let palavraAtual = 0;
+  function escreverTextoH1() {
+    if (palavraAtual < textoH1.length) {
+      if (contadorDeLetras < textoH1[palavraAtual].length) {
+        if (palavraAtual === 1) {
+          tituloElemento.innerHTML += `<span class="texto-colorido">${textoH1[
+            palavraAtual
+          ].charAt(contadorDeLetras)}</span>`;
+        } else {
+          tituloElemento.innerHTML +=
+            textoH1[palavraAtual].charAt(contadorDeLetras);
+        }
+
+        contadorDeLetras++;
+        setTimeout(escreverTextoH1, atraso);
+      } else {
+        palavraAtual++;
+        contadorDeLetras = 0;
+        setTimeout(escreverTextoH1, atraso);
+      }
+    } else {
+      setTimeout(escreverTextoP, atraso);
+    }
+  }
+
+  function escreverTextoP() {
+    if (contadorDeLetras < textoP.length) {
+      paragrafoElemento.textContent += textoP.charAt(contadorDeLetras);
+      contadorDeLetras++;
+      setTimeout(escreverTextoP, atrasoParagrafo);
+    }
+  }
+
+  escreverTextoH1();
+
   document.querySelectorAll("a[href^='#']").forEach((anchor) => {
     anchor.addEventListener("click", function (e) {
       e.preventDefault();
